ead77139d564e4eabb4c07dc1d6b16f2b0ca262c
Durgesh Samariya <samariya.durgesh@gmail.com>
2025-05-14 20:58:43 +1000

    updated db code

--- python/week02/backend/product_service/app/db.py
+++ python/week02/backend/product_service/app/db.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-import time
 
 POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
