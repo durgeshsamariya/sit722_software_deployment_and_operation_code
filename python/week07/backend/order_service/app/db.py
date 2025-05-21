72cff0eeeecb265babdc6fe581392874fcc832a8
Durgesh Samariya <samariya.durgesh@gmail.com>
2025-05-21 12:36:26 +1000

    added init.py.

--- python/week07/backend/order_service/app/db.py
+++ python/week07/backend/order_service/app/db.py
@@ -1,11 +1,26 @@
 # db.py
+
+import os
 import time
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@order_db:5432/orders"
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "postgres")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "order_db")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@"
+    f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
+
+# SQLALCHEMY_DATABASE_URL =
+# "postgresql://postgres:postgres@order_db:5432/orders"
 
 # Retry logic for DB health
 while True:
